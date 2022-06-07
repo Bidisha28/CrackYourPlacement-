e66a9e6e13a5f51a5205f169a08a5fc07ade45ea
Bidisha Mukherjee <67149648+Bidisha28@users.noreply.github.com>
2022-06-07 20:27:11 +0530

    Add files via upload

---
+++ ValidParantheses.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isValid(self, s):
+        while(("()" in s) or ("{}" in s) or ("[]" in s)):
+            if "()" in s:
+                s = s.replace("()", "")
+            if "{}" in s:
+                s = s.replace("{}", "")
+            if "[]" in s:
+                s = s.replace("[]", "")
+        return s == ""
